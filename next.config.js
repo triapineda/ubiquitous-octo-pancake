99a7f1865bda000435387e625baf2dbe04834daf
triapineda <pineda.mapatricia@gmail.com>
2025-08-11 16:12:46 +0800

    commit changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors 'self' https://*.notion.so https://*.notion.site https://notion.so https://notion.site;",
+          },
+          // Non-standard but widely accepted to allow iframes
+          { key: "X-Frame-Options", value: "ALLOWALL" },
+          { key: "Referrer-Policy", value: "no-referrer-when-downgrade" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+        ],
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
